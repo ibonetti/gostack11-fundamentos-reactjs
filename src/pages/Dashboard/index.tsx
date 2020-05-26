0393822108ca5363134795ec9ae94a38df0135df
Israel Bonetti <israelbonetti@MacBook-Pro-de-Israel.local>
2020-05-26 20:06:13 -0300

    transactions

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -40,7 +40,11 @@ const Dashboard: React.FC = () => {
       const formattedTransact = response.data.transactions.map(
         (transaction: Transaction) => ({
           ...transaction,
-          formattedValue: formatValue(transaction.value),
+          formattedValue: formatValue(
+            transaction.type === 'income'
+              ? transaction.value
+              : -transaction.value,
+          ),
           formattedDate: new Date(transaction.created_at).toLocaleDateString(
             'pt-BR',
           ),
@@ -102,18 +106,16 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.formattedValue}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
